ad357d5bab44925a61645ef824bc21b8ac41ad4d
Praveen-Koudagani <69160063+Praveen-Koudagani@users.noreply.github.com>
2021-06-07 07:07:30 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Note(){
+    return (
+         <div className="note">
+             <h1>Javascript and React.js</h1>
+             <p>This was an amazing bootcamp taken up by Shaurya Sinh Sir
+                We covered everything from Scratch including Javascript
+                React.js, HTML.<br></br>
+                Iam praveen as a student happy to attend this Bootcamp to gain knowledge.
+             </p>
+         </div>
+    );
+}
+
+export default Note;
